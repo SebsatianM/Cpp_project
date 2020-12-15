861e0bd382dbf7086abe1df83dadeb13446d6680
SebastianM <56170662+SebsatianM@users.noreply.github.com>
2020-12-15 16:11:01 +0100

    Raport 15.12.2020

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,11 +8,9 @@ MainWindow::MainWindow(QWidget *parent) :
 {
 
     ui->setupUi(this);
-    ui->groupBox->setStyleSheet("QGroupBox::groupBox {subcontrol-position: top;}");
-    mydb =QSqlDatabase::addDatabase("QSQLITE");
-    mydb.setDatabaseName("/home/sebastian/Pulpit/DB/mydb.project");
 
-    if (!mydb.open())
+
+    if (!connOpen())
     {
         ui->db_status_label->setText("Nie połączono z bazą danych");
     }
@@ -20,7 +18,6 @@ MainWindow::MainWindow(QWidget *parent) :
     {
         ui->db_status_label->setText("Prawidłowo połączono z bazą danych");
     }
-
 }
 
 MainWindow::~MainWindow()
@@ -30,11 +27,10 @@ MainWindow::~MainWindow()
 }
 
 
-void MainWindow::on_add_fields_box_clicked()
+void MainWindow::on_add_field_button_clicked()
 {
-
     QString name = ui->f_name_lineEdit->text();
-    QString area = ui->area_lineEdit->text();
+    QString area = ui->f_area_lineEdit->text();
 
     if(name.isEmpty())
         {
@@ -44,19 +40,227 @@ void MainWindow::on_add_fields_box_clicked()
         {
             QMessageBox::information(this, "Błąd", "Nie wypełniono pola 'Powierzchnia'");
         }
-    else if(!mydb.isOpen())
+    else
     {
-            QMessageBox::information(this, "Błąd", "Problem z bazą danych");
+        connOpen();
+        QSqlQuery query;
+        query.prepare("INSERT into fields (name,area) VALUES ('"+name+"','"+area+"')");
+        if(query.exec())
+       {
+           QMessageBox::information(this, tr("Sukces"), tr("Prawidłowo dodano nowe pole"));
+       }
+       else
+       {
+            QMessageBox::critical(this, tr("Błąd"), query.lastError().text());
+       }
+       connClose();
     }
-    QSqlQuery query;
-    query.prepare("INSERT into fields (name,area) VALUES ('"+name+"','"+area+"')");
-    if(query.exec())
-   {
-       QMessageBox::information(this, tr("Sukces"), tr("Prawidłowo dodano nowe pole"));
-   }else
-    {
-        QMessageBox::critical(this, tr("Błąd"), query.lastError().text());
-    }
+}
 
+void MainWindow::on_add_tractor_button_clicked()
+{
+    QString name = ui->t_name_lineEdit->text();
+    QString power = ui->t_power_lineEdit->text();
+
+    if(name.isEmpty())
+        {
+            QMessageBox::information(this, "Błąd", "Nie wypełniono pola 'Nazwa'");
+        }
+    else if (power.isEmpty())
+        {
+            QMessageBox::information(this, "Błąd", "Nie wypełniono pola 'Powierzchnia'");
+        }
+    else
+    {
+    connOpen();
+    QSqlQuery query;
+        query.prepare("INSERT into tractors (name,power) VALUES ('"+name+"','"+power+"')");
+        if(query.exec())
+        {
+            QMessageBox::information(this, tr("Sukces"), tr("Prawidłowo dodano nowe pole"));
+        }
+        else
+        {
+            QMessageBox::critical(this, tr("Błąd"), query.lastError().text());
+        }
+    connClose();
+
+    }
+    loadTractorListToComboBox();
+}
+
+
+void MainWindow::on_tabWidget_tabBarClicked(int index)
+{
+   if (index==2)
+   {
+       loadFieldsToEditList();
+       loadTractorsToEditList();
+   };
+   if (index==3) loadTractorListToComboBox();
+
+}
+
+
+void MainWindow::on_add_wt_button_clicked()
+{
+    QString t_name = ui->wt_t_name_comboBox->currentText();
+    QString name = ui->wt_name_lineEdit->text();
+    QString f_cons = ui->wt_fuel_con_lineEdit->text();
+    qDebug()<<name<<f_cons<<t_name;
+
+    if(name.isEmpty())
+        {
+            QMessageBox::information(this, "Błąd", "Nie wypełniono pola 'Nazwa'");
+        }
+    else if (f_cons.isEmpty())
+        {
+            QMessageBox::information(this, "Błąd", "Nie wypełniono pola 'Zużycie paliwa'");
+        }
+    else
+    {
+    connOpen();
+    QSqlQuery query;
+        query.prepare("INSERT into work_types (name,tractor,fuel_cons) VALUES ('"+name+"','"+t_name+"','"+f_cons+"')");
+        if(query.exec())
+        {
+            QMessageBox::information(this, tr("Sukces"), tr("Prawidłowo dodano nowy rodzaj wykonywanej pracy"));
+        }
+        else
+        {
+            QMessageBox::critical(this, tr("Błąd"), query.lastError().text());
+        }
+    connClose();
+
+    }
+}
+
+
+
+void MainWindow::on_f_name_list_currentIndexChanged(const QString &name)
+{
+    qDebug()<<name;
+    connOpen();
+    QSqlQuery query;
+        query.prepare("SELECT * FROM fields where name='"+name+"'");
+        if(query.exec())
+        {
+            while(query.next())
+            {
+                ui->f_edit_name_lineEdit->setText(query.value(1).toString());
+                ui->f_edit_area_lineEdit->setText(query.value(2).toString());
+                ui->f_edit_desc_lineEdit->setText(query.value(3).toString());
+            }
+
+        }
+        else
+        {
+            QMessageBox::critical(this, tr("Błąd"), query.lastError().text());
+        }
+    connClose();
+}
+
+void MainWindow::on_f_edit_delete_button_clicked()
+{
+    QString name = ui->f_edit_name_lineEdit->text();
+    connOpen();
+    QSqlQuery query;
+        query.prepare("DELETE FROM fields where name='"+name+"'");
+        if(query.exec())
+        {
+            QMessageBox::information(this, tr("Sukces"), tr("Prawidłowo usunięto pole"));
+            loadFieldsToEditList();
+        }
+        else
+        {
+            QMessageBox::critical(this, tr("Błąd"), query.lastError().text());
+        }
+    connClose();
+
+}
+
+void MainWindow::on_f_edit_save_button_clicked()
+{
+    QString base_name = ui->f_name_list->currentText();
+    QString name = ui->f_edit_name_lineEdit->text();
+    QString f_area = ui->f_edit_area_lineEdit->text();
+    QString desc = ui->f_edit_desc_lineEdit->text();
+
+    connOpen();
+    QSqlQuery query;
+        query.prepare("UPDATE fields SET name = '"+name+"',area = '"+f_area+"', description = '"+desc+"' where name='"+base_name+"'");
+        if(query.exec())
+        {
+            QMessageBox::information(this, tr("Sukces"), tr("Prawidłowo zmieniono"));
+            loadFieldsToEditList();
+        }
+        else
+        {
+            QMessageBox::critical(this, tr("Błąd"), query.lastError().text());
+        }
+    connClose();
+
+}
+
+void MainWindow::on_t_name_list_currentIndexChanged(const QString &name)
+{
+    qDebug()<<name;
+    connOpen();
+    QSqlQuery query;
+        query.prepare("SELECT * FROM tractors where name='"+name+"'");
+        if(query.exec())
+        {
+            while(query.next())
+            {
+                ui->t_edit_name_lineEdit->setText(query.value(1).toString());
+                ui->t_edit_power_lineEdit->setText(query.value(2).toString());
+            }
+        }
+        else
+        {
+            QMessageBox::critical(this, tr("Błąd"), query.lastError().text());
+        }
+    connClose();
+}
+
+void MainWindow::on_t_edit_delete_button_clicked()
+{
+    QString name = ui->t_edit_name_lineEdit->text();
+    connOpen();
+    QSqlQuery query;
+        query.prepare("DELETE FROM tractors where name='"+name+"'");
+        if(query.exec())
+        {
+            QMessageBox::information(this, tr("Sukces"), tr("Prawidłowo usunięto traktor"));
+            loadTractorsToEditList();
+        }
+        else
+        {
+            QMessageBox::critical(this, tr("Błąd"), query.lastError().text());
+        }
+    connClose();
+
+}
+
+void MainWindow::on_t_edit_save_button_clicked()
+{
+    QString base_name = ui->t_name_list->currentText();
+    QString name = ui->t_edit_name_lineEdit->text();
+    QString power = ui->t_edit_power_lineEdit->text();
+
+
+    connOpen();
+    QSqlQuery query;
+        query.prepare("UPDATE tractors SET name = '"+name+"', power = '"+power+"' where name='"+base_name+"'");
+        if(query.exec())
+        {
+            QMessageBox::information(this, tr("Sukces"), tr("Prawidłowo zmieniono"));
+            loadTractorsToEditList();
+        }
+        else
+        {
+            QMessageBox::critical(this, tr("Błąd"), query.lastError().text());
+        }
+    connClose();
 
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,7 +4,8 @@
 #include <QMainWindow>
 #include <QtSql>
 #include <QFileInfo>
-#include "connection.h"
+#include <QDebug>
+#include "ui_mainwindow.h"
 namespace Ui {
 class MainWindow;
 }
@@ -14,16 +15,108 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    QSqlDatabase mydb;
+    void connClose()
+    {
+        mydb.close();
+        mydb.removeDatabase(QSqlDatabase::defaultConnection);
+    }
+    bool connOpen()
+    {
+        mydb =QSqlDatabase::addDatabase("QSQLITE");
+        mydb.setDatabaseName("/home/sebastian/Pulpit/DB/mydb.project");
+
+        if (!mydb.open())
+        {
+            qDebug()<<("Nie połączono z bazą danych");
+            return false;
+        }
+        else
+        {
+             qDebug()<<("Prawidłowo połączono z bazą danych");
+             return true;
+        }
+    }
+    void loadTractorListToComboBox()
+    {
+        connOpen();
+
+        QSqlQueryModel *modal=new QSqlQueryModel();
+        QSqlQuery* query = new QSqlQuery(mydb);
+
+        query->prepare("SELECT name from tractors");
+        query->exec();
+        modal->setQuery(*query);
+        ui->wt_t_name_comboBox->setModel(modal);
+
+        connClose();
+
+        qDebug()<<(modal->rowCount());
+
+
+    }
+    void loadFieldsToEditList()
+    {
+        connOpen();
+
+        QSqlQueryModel *modal=new QSqlQueryModel();
+        QSqlQuery* query = new QSqlQuery(mydb);
+
+        query->prepare("SELECT name from fields");
+        query->exec();
+
+        modal->setQuery(*query);
+        ui->f_name_list->setModel(modal);
+
+        connClose();
+
+        qDebug()<<(modal->rowCount());
+    }
+    void loadTractorsToEditList()
+    {
+        connOpen();
+
+        QSqlQueryModel *modal=new QSqlQueryModel();
+        QSqlQuery* query = new QSqlQuery(mydb);
+
+        query->prepare("SELECT name from tractors");
+        query->exec();
+        modal->setQuery(*query);
+        ui->t_name_list->setModel(modal);
+        connClose();
+
+        qDebug()<<(modal->rowCount());
+    }
+
+public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
 private slots:
 
-    void on_add_fields_box_clicked();
+    void on_add_tractor_button_clicked();
+
+    void on_add_field_button_clicked();
+
+    void on_tabWidget_tabBarClicked(int index);
+
+    void on_add_wt_button_clicked();
+
+    void on_f_name_list_currentIndexChanged(const QString &arg1);
+
+    void on_f_edit_delete_button_clicked();
+
+    void on_f_edit_save_button_clicked();
+
+    void on_t_name_list_currentIndexChanged(const QString &arg1);
+
+    void on_t_edit_delete_button_clicked();
+
+    void on_t_edit_save_button_clicked();
 
 private:
     Ui::MainWindow *ui;
-    QSqlDatabase mydb;
+
 };
 
 #endif // MAINWINDOW_H
